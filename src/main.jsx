64dbabae321c00436e5b937c1d44b78c86708c34
Jota <jvfranco08@gmail.com>
2024-09-21 01:09:14 -0300

    tinha esquecido das rotas do quiz, news e live, arrumado!!

--- src/main.jsx
+++ src/main.jsx
@@ -12,22 +12,28 @@ import Error from './routes/Error.jsx'
 import Pilots from './routes/Pilots.jsx'
 import Login from './routes/Login.jsx'
 import Cadastro from './routes/Cadastro.jsx'
+import Quiz from './routes/Quiz.jsx'
+import News from './routes/News.jsx'
+import Live from './routes/Live.jsx'
 
 const router = createBrowserRouter([
   {
-    path:'/',element:<App/>,
-    errorElement:<Error/>,
+    path: '/', element: <App />,
+    errorElement: <Error />,
 
-    children:[
-      {path:'/',element:<Home/>},
-      {path:'/Teams',element:<Teams/>},
-      {path:'/Circuits',element:<Circuits/>},
-      {path:'/Races',element:<Races/>},
-      {path:'/Pilots',element:<Pilots/>},
-      {path:'/Login',element:<Login/>},
-      {path:'/Cadastro',element:<Cadastro/>}, 
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/Teams', element: <Teams /> },
+      { path: '/Circuits', element: <Circuits /> },
+      { path: '/Races', element: <Races /> },
+      { path: '/Pilots', element: <Pilots /> },
+      { path: '/Login', element: <Login /> },
+      { path: '/Cadastro', element: <Cadastro /> },
+      { path: '/News', element: <News /> },
+      { path: '/Live', element: <Live /> },
+      { path: '/Quiz', element: <Quiz /> },
     ]
-}
+  }
 ])
 
 
