3e019f6ce8195820a88d25d5d75e462d607d6756
Jota <jvfranco08@gmail.com>
2024-09-25 14:23:42 -0300

    news em progresso, vou pra academia

--- src/routes/News.jsx
+++ src/routes/News.jsx
@@ -21,7 +21,8 @@ const carouselNews = [
   {
     id: 3,
     title: "Tecnologia de carros elétricos em expansão",
-    image: "https://cdn-3.motorsport.com/images/amp/YMdmy4g2/s1000/formula-e-gen-3-evo.jpg",
+    image:
+      "https://cdn-3.motorsport.com/images/amp/YMdmy4g2/s1000/formula-e-gen-3-evo.jpg",
     description:
       "Os avanços na tecnologia de carros elétricos trazem benefícios tanto para a Fórmula E quanto para o mercado automobilístico.",
   },
@@ -41,7 +42,8 @@ const newsList = [
   {
     id: 2,
     title: "Pilotos se Preparam para o ePrix de Nova York",
-    image: "https://resources.formula-e.pulselive.com/photo-resources/2024/07/04/01cae071-704f-4170-bb27-a665d5aba564/1020259395-LAT-20240629-EPS1013_130619_FC14743.jpg?width=544&height=308",
+    image:
+      "https://resources.formula-e.pulselive.com/photo-resources/2024/07/04/01cae071-704f-4170-bb27-a665d5aba564/1020259395-LAT-20240629-EPS1013_130619_FC14743.jpg?width=544&height=308",
     description:
       "Os pilotos estão em preparação intensa para a corrida que ocorrerá nas ruas de Nova York.",
     content:
@@ -50,7 +52,8 @@ const newsList = [
   {
     id: 3,
     title: "Mahindra Revela Novo Carro para a Temporada",
-    image: "https://e-formula.news/files/e-formel/uploads/_NEWS/2024/09%20September/Mahindra-Gen3-Evo-car-testing.jpg",
+    image:
+      "https://e-formula.news/files/e-formel/uploads/_NEWS/2024/09%20September/Mahindra-Gen3-Evo-car-testing.jpg",
     description:
       "Mahindra revelou seu novo modelo 'Mahindra M10Electro' para a próxima temporada.",
     content: "Com um design mais aerodinâmico e melhoras no desempenho...",
@@ -58,7 +61,8 @@ const newsList = [
   {
     id: 4,
     title: "Análise: A Evolução da Fórmula E ao Longo dos Anos",
-    image: "https://resources.formula-e.pulselive.com/photo-resources/2023/12/19/8de8a274-1b25-4c71-9e93-d642f63e3830/1018732629-LAT-20210724-EPS712_140310SB3_9236-min.jpg?width=544&height=308",
+    image:
+      "https://resources.formula-e.pulselive.com/photo-resources/2023/12/19/8de8a274-1b25-4c71-9e93-d642f63e3830/1018732629-LAT-20210724-EPS712_140310SB3_9236-min.jpg?width=544&height=308",
     description:
       "Desde o início da Fórmula E, a tecnologia e a competitividade evoluíram drasticamente.",
     content:
@@ -118,113 +122,123 @@ const RightColumn = styled.div`
 // Estilos do carrossel
 const CarouselStyle = styled.div`
   .carousel-container {
-  width: 100%; /* Largura relativa ao contêiner pai */
-  max-width: 60vw; /* Tamanho máximo para telas maiores */
-  height: 24vw;
-  margin: auto;
-  overflow: hidden;
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  border-radius: 8px;
-}
-
-.carousel {
-  width: 100%;
-  height: 85%;
-  background-size: cover; /* Redimensiona a imagem para cobrir o contêiner */
-  background-position: center; /* Centraliza a imagem */
-  position: relative;
-  display: flex;
-  align-items: end;
-  justify-content: start;
-}
-
-.overlay {
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: rgba(0, 0, 0, 0.6); /* Filtro escuro */
-}
-
-.carousel-content {
-  position: relative;
-  color: white;
-  padding: 0.5rem;
-  z-index: 1;
-  h2{
-    font-size: clamp(0.8rem, 1.4vw, 2rem);
-    padding: 0vw 2vw;
+    width: 100%; /* Largura relativa ao contêiner pai */
+    max-width: 60vw; /* Tamanho máximo para telas maiores */
+    height: 24vw;
+    margin: auto;
+    overflow: hidden;
+    position: relative;
     display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    border-radius: 8px;
+    border: 4px solid var(--opacity);
   }
-  p{
-    font-size: clamp(0.2rem, 0.8vw, 1.2rem);
+
+  .carousel {
+    width: 100%;
+    height: 85%;
+    background-size: cover; /* Redimensiona a imagem para cobrir o contêiner */
+    background-position: center; /* Centraliza a imagem */
+    position: relative;
     display: flex;
-    padding: 1vw 2vw;
-  }
-}
-
-.carousel-controls {
-  display: flex;
-  justify-content: center;
-  padding: 1vw 0;
-  background-color: white;
-}
-
-.carousel-controls button {
-  margin: 0 0.25rem;
-  padding: 0.5vw;
-  border: none;
-  background-color: var(--opacity);
-  border: 2px solid var(--opacity);
-  cursor: pointer;
-  border-radius: 4px;
-}
-
-.carousel-controls button.active {
-  background-color: var(--color1);
-  color: white;
-}
-
-/* Responsividade */
-@media (max-width: 768px) {
-  .carousel-container {
-    height: 250px; /* Ajusta a altura para tablets e telas menores */
+    align-items: end;
+    justify-content: start;
+    opacity: 1;
+    transition: opacity 0.5s ease-in-out; /* Transição suave de 0.5 segundos */
   }
 
-  .carousel-content h2 {
-    font-size: 1.25rem; /* Ajusta o tamanho do título */
-  }
-
-  .carousel-content p {
-    font-size: 0.9rem; /* Ajusta o tamanho do texto */
-  }
-}
-
-@media (max-width: 480px) {
-  .carousel-container {
-    height: 200px; /* Ajusta a altura para smartphones */
+  .overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0, 0, 0, 0.6); /* Filtro escuro */
   }
 
   .carousel-content {
-    h2{
-    font-size: 0.8rem;
-    font-weight: 900;
+    position: relative;
+    color: white;
+    padding: 0.5rem;
+    z-index: 1;
+    h2 {
+      font-size: clamp(0.8rem, 1.4vw, 2rem);
+      padding: 0vw 2vw;
+      display: flex;
+    }
+    p {
+      font-size: clamp(0.2rem, 0.8vw, 1.2rem);
+      display: flex;
+      padding: 1vw 2vw;
+    }
   }
-  p{
-    margin-top: 4px;
-    font-size: 0.6rem;
-  }
+
+  .carousel-controls {
+    display: flex;
+    justify-content: center;
+    padding: 1vw 0;
+    background-color: white;
+    border-top: 2px solid var(--opacity);
   }
 
   .carousel-controls button {
-    padding: 0.3rem; /* Diminui os botões de controle */
+    margin: 0 0.25rem;
+    padding: 0.5vw;
+    border: none;
+    background-color: var(--opacity);
+    border: 2px solid var(--opacity);
+    cursor: pointer;
+    border-radius: 4px;
   }
+
+  .carousel-controls button.active {
+    background-color: var(--color1);
+    color: white;
+  }
+
+  /* Responsividade */
+  @media (max-width: 768px) {
+    .carousel-container {
+      height: 250px; /* Ajusta a altura para tablets e telas menores */
+    }
+
+    .carousel-content h2 {
+      font-size: 1.25rem; /* Ajusta o tamanho do título */
+    }
+
+    .carousel-content p {
+      font-size: 0.9rem; /* Ajusta o tamanho do texto */
+    }
+  }
+
+  @media (max-width: 480px) {
+    .carousel-container {
+      height: 200px; /* Ajusta a altura para smartphones */
+    }
+
+    .carousel-content {
+      h2 {
+        font-size: 0.8rem;
+        font-weight: 900;
+      }
+      p {
+        margin-top: 4px;
+        font-size: 0.6rem;
+      }
+    }
+
+    .carousel-controls button {
+      padding: 0.3rem; /* Diminui os botões de controle */
+    }
+    .carousel.fade-in {
+  opacity: 1; /* Aparece suavemente */
 }
 
+.carousel.fade-out {
+  opacity: 0; /* Some suavemente */
+}
+  }
 `;
 
 //outras noticias abaixo do carrossel
@@ -246,16 +260,15 @@ const NewsCard = styled.div`
 
 const NewsImage = styled.img`
   width: 100%;
-  max-width: 550px;
-  height: 308px;  
-  object-fit: cover;
+  aspect-ratio: 5/3; // Proporção da imagem
+  object-fit: cover; // Redimensiona a imagem para cobrir o contêiner
   overflow: hidden;
   border-radius: 8px;
 `;
 
 const NewsTitle = styled.h3`
   margin-top: 10px;
-  font-size: 1.5rem;
+  font-size: 1.2rem;
 `;
 
 const NewsDescription = styled.p`
@@ -283,16 +296,21 @@ const RightNewsDescription = styled.p`
 
 const NewsPage = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [fade, setFade] = useState(true); // Controle de opacidade para a transição
 
   // Efeito para mudar automaticamente a imagem a cada 3 segundos
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) =>
-        prevIndex === carouselNews.length - 1 ? 0 : prevIndex + 1
-      );
+      setFade(false); // Inicia a transição de saída
+      setTimeout(() => {
+        setCurrentIndex((prevIndex) =>
+          prevIndex === carouselNews.length - 1 ? 0 : prevIndex + 1
+        );
+        setFade(true); // Inicia a transição de entrada
+      }, 500); // Tempo da transição (mesmo que o CSS)
     }, 3000);
 
-    return () => clearInterval(interval); // Limpa o intervalo ao desmontar o componente
+    return () => clearInterval(interval);
   }, []);
 
   return (
@@ -303,7 +321,7 @@ const NewsPage = () => {
         <CarouselStyle>
           <div className="carousel-container">
             <div
-              className="carousel"
+              className={`carousel ${fade ? "fade-in" : "fade-out"}`}
               style={{
                 backgroundImage: `url(${carouselNews[currentIndex].image})`,
               }}
