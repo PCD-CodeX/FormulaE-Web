77c556648e68ef437f3f1a10a134219354d4de3f
Jota <jvfranco08@gmail.com>
2024-09-26 10:33:16 -0300

    style de circuitos ajustado

--- src/routes/Circuits.jsx
+++ src/routes/Circuits.jsx
@@ -1,24 +1,25 @@
 import styled from 'styled-components';
+import { useState } from 'react';
 
 // Lista de pistas com os continentes
 const circuits = [
-  { id: 1, sedePT: "Berlim", track: 'germany', flagCountry: 'de', color: 'black , #d52c1e , #ecc000', continent: 'Europa' },
-  { id: 2, sedePT: "Buenos Aires", track: 'argentina', flagCountry: 'ar', color: '#74acde , #f4b41a , white', continent: 'América' },
-  { id: 3, sedePT: "Cidade do Mexico", track: 'mexico', flagCountry: 'mx', color: ' #1e9f45 , #d20100 , white', continent: 'América' },
-  { id: 4, sedePT: "Londres", track: 'uk', flagCountry: 'gb', color: '#74acde , #f4b41a , white', continent: 'Europa' },
-  { id: 5, sedePT: "Madrid", track: 'spain', flagCountry: 'es', color: '#c7291c , #e7be00 , #c7291c', continent: 'Europa' },
-  { id: 6, sedePT: "Miami", track: 'usa', flagCountry: 'us', color: '#d52c1e , #1c34ae , white', continent: 'América' },
-  { id: 7, sedePT: "Monaco", track: 'monaco', flagCountry: 'mc', color: '#d30002 , white', continent: 'Europa' },
-  { id: 8, sedePT: "Montreal", track: 'canada', flagCountry: 'ca', color: ' #b92922 , #d52c1e', continent: 'América' },
-  { id: 9, sedePT: "Moscou", track: 'russia', flagCountry: 'ru', color: '#0139a5 , #d52a1e', continent: 'Europa' },
-  { id: 10, sedePT: "Nova Delhi", track: 'india', flagCountry: 'in', color: '#ff9935 , #138808 , white', continent: 'Ásia' },
-  { id: 11, sedePT: "Paris", track: 'france', flagCountry: 'fr', color: '#74acde , #f4b41a , white', continent: 'Europa' },
-  { id: 12, sedePT: "Roma", track: 'italy', flagCountry: 'it', color: '#ce2d35 , white', continent: 'Europa' },
-  { id: 13, sedePT: "Santiago", track: 'chile', flagCountry: 'cl', color: '#ce2a28 , #2806e5', continent: 'América' },
-  { id: 14, sedePT: "São Paulo", track: 'brazil', flagCountry: 'br', color: '#239e46 , #2b49a3 , #eddc26', continent: 'América' },
-  { id: 15, sedePT: "Shangai", track: 'china', flagCountry: 'cn', color: '#74acde , #f4b41a , white', continent: 'Ásia' },
-  { id: 16, sedePT: "Tokyo", track: 'japan', flagCountry: 'jp', color: '#74acde , #f4b41a , white', continent: 'Ásia' },
-  { id: 17, sedePT: "Zurich", track: 'swiss', flagCountry: 'sw', color: ' white, #d30002', continent: 'Europa' },
+  { id: 1, sedePT: "Berlim", track: 'germany', flagCountry: 'Alemanha', continent: 'Europa' },
+  { id: 2, sedePT: "Buenos Aires", track: 'argentina', flagCountry: 'Argentina',  continent: 'América' },
+  { id: 3, sedePT: "Cidade do Mexico", track: 'mexico', flagCountry: 'México',  continent: 'América' },
+  { id: 4, sedePT: "Londres", track: 'uk', flagCountry: 'Reino Unido', continent: 'Europa' },
+  { id: 5, sedePT: "Madrid", track: 'spain', flagCountry: 'Espanha', continent: 'Europa' },
+  { id: 6, sedePT: "Miami", track: 'usa', flagCountry: 'EUA', continent: 'América' },
+  { id: 7, sedePT: "Monaco", track: 'monaco', flagCountry: 'Mônaco',  continent: 'Europa' },
+  { id: 8, sedePT: "Montreal", track: 'canada', flagCountry: 'Canada', continent: 'América' },
+  { id: 9, sedePT: "Moscou", track: 'russia', flagCountry: 'Rússia',  continent: 'Europa' },
+  { id: 10, sedePT: "Nova Delhi", track: 'india', flagCountry: 'Índia',continent: 'Ásia' },
+  { id: 11, sedePT: "Paris", track: 'france', flagCountry: 'França',  continent: 'Europa' },
+  { id: 12, sedePT: "Roma", track: 'italy', flagCountry: 'Italia', continent: 'Europa' },
+  { id: 13, sedePT: "Santiago", track: 'chile', flagCountry: 'Chile', continent: 'América' },
+  { id: 14, sedePT: "São Paulo", track: 'brazil', flagCountry: 'Brasil', continent: 'América' },
+  { id: 15, sedePT: "Shangai", track: 'china', flagCountry: 'China', continent: 'Ásia' },
+  { id: 16, sedePT: "Tokyo", track: 'japan', flagCountry: 'Japão', continent: 'Ásia' },
+  { id: 17, sedePT: "Zurich", track: 'swiss', flagCountry: 'Suiça', continent: 'Europa' },
 ];
 
 // Função que agrupa as pistas por continente
@@ -33,79 +34,152 @@ const groupByContinent = (circuits) => {
   }, {});
 };
 
-// Estilos de componentes
+// Estilos para o card
 const CircuitCard = styled.div`
-    border-radius: 10px;
+  perspective: 1000px;
+  margin: 2rem;
+  width: 300px;
+  height: 400px;
+  position: relative;
+
+  .card-inner {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    text-align: center;
+    transition: transform 0.8s ease-in-out;
+    transform-style: preserve-3d;
+    transform: ${({ flipped }) => (flipped ? 'rotateY(180deg)' : 'rotateY(0deg)')};
+  }
+
+  .card-front,
+  .card-back {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    backface-visibility: hidden;
+    border-radius: 15px;
+    box-shadow: 0 8px 20px rgba(0, 0, 0, 0.15);
+  }
+
+  .card-front {
+    background: linear-gradient(145deg, rgba(3, 83, 121, 0.8) 20%, rgba(154, 177, 255, 0.7));
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 2rem;
     color: white;
-    font-size: 24px;
-    text-align: center;
-    background: linear-gradient(125deg, rgb(4, 175, 255) 60%, white 100%);
-    margin: 4vh;
-    padding: 4vh;
-    text-align: center;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    min-width: 16vw;
-    transition: all 0.3s ease;
-    h2 {
-    font-size: clamp(14px, 1.2vw, 26px);
+  }
+
+  .card-back {
+    background: linear-gradient(145deg, rgba(0, 71, 255, 0.8), rgba(255, 255, 255, 0.7));
+    transform: rotateY(180deg);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 2rem;
+  }
+
+  h2 {
+    font-size: 1.5rem;
     font-weight: bold;
-    margin-bottom: 4vh;
-    letter-spacing: 3px;
-    font-weight: 900;
-    padding: 2vh;
-    border-radius: 4px;
-    }
-    .images {
+    margin-bottom: 1rem;
+    letter-spacing: 2px;
+  }
+
+  .images {
     display: flex;
     flex-direction: column;
     align-items: center;
-    }
-  &:hover {
-    scale: 1.02;
+  }
+
+  .back-info {
+    font-size: 1.2rem;
+    color: white;
+    text-align: center;
   }
 `;
 
 const CircuitImage = styled.img`
   width: 100%;
-  max-width: 300px;
+  max-width: 250px;
   height: auto;
-  margin-bottom: 4vh;
+  margin-bottom: 1.5rem;
   border-radius: 10px;
-  filter: drop-shadow(0 0px 8px rgba(0, 0, 0, 0.4));
+  transition: filter 0.3s ease;
+`;
+
+const FlipButton = styled.button`
+ display: flex;
+    justify-content: center;
+    padding: clamp(6px,1vw,30px);
+    border-radius: 1vw;
+    margin-top: 1vh;
+    background-color: var(--opacity);
+    border: clamp(2px,0.2vw,4px) solid var(--opacity) ;
+    font-size: clamp(16px, 0.9vw, 20px);
+    transition: 0.2s ease;  
+    font-style: none;
+    text-decoration: none;
+    p{
+            color: white;
+            font-weight: 800;
+    }
+
+    &:hover{
+    background-color: var(--opacity);
+    border: 0.2vw solid var(--color3) ;
+    scale: 1.03;
+}
 `;
 
 const GridContainer = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
   grid-auto-rows: auto;
-  grid-template-rows: repeat(2, 1fr); 
-  gap: 4vw 10vw;
-  padding: 4vw 14vw 0vw 14vw;
-  filter: drop-shadow(0 0px 8px rgba(0, 0, 0, 0.3));
+  gap: 4rem;
+  padding: 6vw 8vw;
 `;
 
 const ContinentContainer = styled.div`
-    //background: linear-gradient(90deg, rgb(4, 66, 95) 5%, var(--color1) 100%);
-    padding-top: 10vh; 
+  padding-top: 5vh;
 
-    .continent{
+  .continent {
     display: flex;
     justify-content: center;
-    font-size: clamp(20px, 2vw, 36px);
+    font-size: clamp(1.5rem, 3vw, 2.5rem);
     font-weight: bold;
-    margin-bottom: 4vh;
+    margin-bottom: 3vh;
     letter-spacing: 3px;
-    font-weight: 900;
     background: var(--color2);
-    padding: 2vh;
-    color: white; 
-    margin: 0vh clamp(10px,40vw,1100px);
-    border-radius: 30px;
-    }
+    padding: 1rem;
+    color: white;
+    border-radius: 20px;
+    box-shadow: 0 4px 12px rgba(0, 0, 0, 0.2);
+    transition: all 0.3s ease;
+    margin: 0 32vw;
+  }
+
+  .continent:hover {
+    transform: translateY(-5px);
+  }
 `;
 
 const CircuitsList = () => {
   const circuitsByContinent = groupByContinent(circuits);
+
+  // Controla o estado de flip para cada card
+  const [flippedCards, setFlippedCards] = useState({});
+
+  // Função para alternar o estado de flip de um card específico
+  const handleFlip = (id) => {
+    setFlippedCards((prevState) => ({
+      ...prevState,
+      [id]: !prevState[id],
+    }));
+  };
 
   return (
     <div>
@@ -114,14 +188,29 @@ const CircuitsList = () => {
           <h2 className='continent'>{continent}</h2>
           <GridContainer>
             {circuitsByContinent[continent].map((circuit) => (
-              <CircuitCard key={circuit.id} color={circuit.color}>
-                <div className="hover">
-                  <h2>{circuit.sedePT}</h2>
-                  <div className="images">
-                    <CircuitImage
-                      src={`/tracks/${circuit.track}.png`}
-                      alt={`Imagem da pista de ${circuit.sedePT}`}
-                    />
+              <CircuitCard
+                key={circuit.id}
+                flipped={flippedCards[circuit.id]}
+                onClick={() => handleFlip(circuit.id)}
+              >
+                <div className="card-inner">
+                  <div className="card-front">
+                    <h2>{circuit.sedePT}</h2>
+                    <div className="images">
+                      <CircuitImage
+                        src={`/tracks/${circuit.track}.png`}
+                        alt={`Imagem da pista de ${circuit.sedePT}`}
+                      />
+                    </div>
+                    <FlipButton>
+                      <p>Clique aqui</p> 
+                    </FlipButton>
+                  </div>
+                  <div className="card-back">
+                    <div className="back-info">
+                      <h2>{circuit.sedePT}</h2>
+                      <p>País: {circuit.flagCountry}</p>
+                    </div>
                   </div>
                 </div>
               </CircuitCard>
